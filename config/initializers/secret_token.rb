5a0ad697112f21c50716626a35bd29ca22b6ac84
Marco Salazar <salazarm@mit.edu>
2013-06-15 13:15:37 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voiceapp::Application.config.secret_token = 'bac33687fd0378cc00f552b56565b663477fbd43d3064e99c8d302535665568188373c229217c66bae396fca1e11b2c7bcfbd34e76547cec550039f2071a7501'
