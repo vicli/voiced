6b5decbf567424b56db521443b86652926d0b88c
Marco Salazar <salazarm@mit.edu>
2013-06-15 19:28:09 -0600

    s

--- app/controllers/reminder_controller.rb
+++ app/controllers/reminder_controller.rb
@@ -1,6 +1,7 @@
 class ReminderController < ApplicationController
+
 	def create
-		@reminder Reminder.create(params[:reminder])
+		@reminder = Reminder.create(params[:reminder])
 		render :json => @reminder
 	end		
 end
