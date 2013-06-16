8851c64a513c441be4ecb99b5cc3ef4d1f5956a7
Victoria Li <victoli.d@gmail.com>
2013-06-16 08:36:01 -0700

    extension

--- public/recorder.js
+++ public/recorder.js
@@ -78,7 +78,7 @@
       type: "GET",
       url: url,
       success: function(data, status, jqXHR){
-        filepicker.store(data, {filename: filename + '.wav', mimetype: 'audio/xwave'}, function(fpfile){
+        filepicker.store(data, {filename: filename + '.wav', extension: '.wav'}, function(fpfile){
           $("#new_reminder").prepend("<input type=\"hidden\" name=\"reminder[file]\" value=\""+fpfile.url+"\"/>");
 
         });
