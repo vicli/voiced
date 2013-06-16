713e5ae5427c86b22e49a8aae10fffd916d7502a
Marco Salazar <salazarm@mit.edu>
2013-06-16 11:13:00 -0600

    test'

--- public/recorder.js
+++ public/recorder.js
@@ -78,8 +78,9 @@
       type: "GET",
       url: url,
       success: function(data, status, jqXHR){
-        filepicker.store(data, {filename: filename, mimetypes: ['audio/*']}, function(fpfile){
-          $("#new_reminder").prepend("<input type=\"hidden\" name=\"reminder[file]\" value=\""+fpfile.url + "\"/>");
+        filepicker.store(data, {filename: filename + '.wav'}, function(fpfile){
+          $("#new_reminder").prepend("<input type=\"hidden\" name=\"reminder[file]\" value=\""+fpfile.url+"\"/>");
+
         });
       }
     })
